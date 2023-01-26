928ceece74e96d6bd51bd5bdd14b00e8d17fd0a2
jccintr <jccintr@gmail.com>
2023-01-26 19:38:38 -0400

    criado layout tela login

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import styles from "./styles.module.css";
+import logo from "../../assets/logo-absentor-310x220.png";
+import { FaRegUser } from "react-icons/fa";
+import {IoKeyOutline} from "react-icons/io5";
+
+const Login = () => {
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+
+  return (
+    <div className={styles.container}>
+        <img className={styles.logo} src={logo} alt="Logo Absentor" />
+        <h2>Dados de Acesso</h2>
+        <div className={styles.blueline}></div>
+
+        <div className={styles.containerInput}>
+         <FaRegUser className={styles.icon} size={18} /> 
+          <input
+            className={styles.inputPesquisa}
+            placeholder="Usuário"
+            type="text"
+            onChange={(e)=>setEmail(e.target.value)}
+            value={email}
+          />
+        </div>
+
+        <div className={styles.containerInput}>
+         <IoKeyOutline className={styles.icon} size={20} /> 
+          <input
+            className={styles.inputPesquisa}
+            placeholder="Senha"
+            type="password"
+            onChange={(e)=>setPassword(e.target.value)}
+            value={password}
+          />
+        </div>
+
+        <button onClick={()=>alert('Tocou no botão Entrar')}className={styles.botao}>Entrar</button>
+    </div>
+  )
+}
+
+export default Login
